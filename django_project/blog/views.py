6db71f80b9d2255412484021e74c4d6047ef08a5
Bharani Deepak <deepakbharani@gmail.com>
2021-05-08 15:28:57 +0200

    db models added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'Bharani',
-		'title': 'FEM',
-		'content': 'Enkitta modhadha da',
-		'date_posted': 'May 06, 2021'
-	},
-	{
-		'author': 'Anusu',
-		'title': 'Finance',
-		'content': 'Va de muniyamma',
-		'date_posted': 'May 05, 2021'
-	}
-]
+from .models import Post
 
 def home(request):
 	context ={
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html',context)
 
