d3b906ada0f8236a4b9ef8132e89f3a2b186309a
Bharani Deepak <deepakbharani@gmail.com>
2021-05-14 17:47:07 +0200

    delte and update views added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 	ListView, 
 	DetailView,
-	CreateView)
+	CreateView,
+	UpdateView,
+	DeleteView
+	)
 from .models import Post
 
 
@@ -25,6 +28,7 @@ class PostDetailView(DetailView):
 	# model variable will define what model to query to create the kist	
 	model = Post
 
+# LoginRequiredMixin - Makes sure that the user has logged in
 class PostCreateView(LoginRequiredMixin, CreateView):
 	# model variable will define what model to query to create the kist	
 	model = Post
@@ -33,6 +37,38 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+# UserPassesTestMixin - Make sures that the user who has created the post has the 
+# power to update of delete the post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	# model variable will define what model to query to create the kist	
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		# self.get_object() will get the post we want to edit
+		post = self.get_object()
+		# self.request.user = logged in user
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+	# model variable will define what model to query to create the kist	
+	model = Post
+	success_url = '/'
+
+	def test_func(self):
+		# self.get_object() will get the post we want to edit
+		post = self.get_object()
+		# self.request.user = logged in user
+		if self.request.user == post.author:
+			return True
+		return False
 
 def about(request):
 	return render(request, 'blog/about.html',{'title': 'About'})
